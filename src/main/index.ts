3c4c1bc75b2cb5add33b4a99b1f98b13bcef57bb
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-10-16 17:34:01 +0200

    updated project

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,12 +1,14 @@
-import { app, BrowserWindow } from 'electron';
-import { join } from 'path';
+import { app, BrowserWindow, dialog } from "electron";
+import { join } from "path";
+// import { readFile } from "fs/promises";
+import { readFile } from "node:fs/promises";
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: join(__dirname, 'preload.js'),
+      preload: join(__dirname, "preload.js"),
     },
   });
 
@@ -14,25 +16,49 @@ const createWindow = () => {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
     mainWindow.loadFile(
-      join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
+      join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
     );
   }
 
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+    mainWindow.focus();
+  });
+
   mainWindow.webContents.openDevTools({
-    mode: 'detach',
+    mode: "detach",
   });
 };
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
+
+const showOpenDialog = async (browserWindow: BrowserWindow) => {
+  const result = await dialog.showOpenDialog(browserWindow, {
+    properties: ["openFile"],
+    filters: [{ name: "Markdown File", extensions: ["md"] }],
+  });
+
+  if (result.canceled) return;
+
+  const [filePath] = result.filePaths;
+
+  openFile(browserWindow, filePath);
+};
+
+const openFile = async (browserWindow: BrowserWindow, filePath: string) => {
+  const content = await readFile(filePath, { encoding: "utf-8" });
+
+  browserWindow.webContents.send("file-opened", filePath, content);
+};
